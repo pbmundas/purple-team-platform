32eb4cabb0e7f5596bb9e983933f374dd2886682
pbmundas <prasanna.mundas@gmail.com>
2026-07-14 16:01:55 +0530

    first commit delete copy

--- kali - Copy/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -eu
-echo 'Purple-team Kali ready. Authorized lab targets only.'
-exec tail -f /dev/null
--- victim-linux - Copy/victim-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-mkdir -p /var/log/purple
-touch /var/log/purple/auth.log
-(nginx 2>/dev/null || true)
-exec /init
